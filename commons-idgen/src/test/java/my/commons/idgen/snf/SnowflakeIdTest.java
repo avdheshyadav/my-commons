f4f611663b3cc6d078b30af95707ecfa4ffc0601
Avdhesh <avdy405@gmail.com>
2024-11-03 16:44:57 +0530

    fix

---
+++ commons-idgen/src/test/java/my/commons/idgen/snf/SnowflakeIdTest.java
@@ -0,0 +1,11 @@
+package my.commons.idgen.snf;
+
+public class SnowflakeIdTest {
+
+    public static void main(String[] args) {
+        System.out.println("---- Inside snowflake Id Test Enter------");
+
+
+        System.out.println("---- Inside snowflake Id Test Exit------");
+    }
+}
