1e701934848d5e3c0065a1afac63d57ab31dc66f
Avdhesh <avdy405@gmail.com>
2024-11-06 21:38:12 +0530

    fix

--- commons-idgen/src/test/java/my/commons/idgen/snf/SnowflakeIdTest.java
+++ commons-idgen/src/test/java/my/commons/idgen/snf/SnowflakeIdTest.java
@@ -1,60 +1,49 @@
 package my.commons.idgen.snf;
 
-import my.commons.idgen.snf.impl.DefaultSnfIdConfigImpl;
-import my.commons.idgen.snf.impl.MachineId;
-import my.commons.idgen.snf.impl.NetworkInterfaceMachineIdProvider;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class SnowflakeIdTest {
 
     public static void main(String[] args) throws Exception {
         System.out.println("---- Inside snowflake Id Test Enter------");
-        //basic();
         initTest();
         System.out.println("---- Inside snowflake Id Test Exit------");
     }
 
     public static void initTest() throws Exception {
         CountDownLatch idInitLatch = new CountDownLatch(1);
-        int noOfIds = 10000;
+        int noOfIds = 10;
         ExecutorService threadPool = Executors.newVirtualThreadPerTaskExecutor();
         ArrayList<Future<ArrayList<SnfId>>> futures = new ArrayList<>();
-        for(int i = 0; i < 5; i++) {
-            Future<ArrayList<SnfId>> future = threadPool.submit(new IdGenInitializationWorker("Worker " + i , idInitLatch, noOfIds ));
+        for (int i = 0; i < 10; i++) {
+            Future<ArrayList<SnfId>> future = threadPool.submit(new IdGenInitializationWorker("Worker " + i, idInitLatch, noOfIds));
             futures.add(future);
         }
         Thread.sleep(2000);
         idInitLatch.countDown();
 
-        ArrayList<Long> masterList = new ArrayList<>();
-        ArrayList<Long> duplicates = new ArrayList<>();
-
-        for(Future<ArrayList<SnfId>> future : futures) {
+        ArrayList<SnfId> masterList = new ArrayList<>();
+        for (Future<ArrayList<SnfId>> future : futures) {
             ArrayList<SnfId> ids = future.get();
-            for(SnfId snfId : ids){
-                Long id = SnfIdGen.getId(snfId);
-                if(masterList.contains(id)){
-                    duplicates.add(id);
-                }
-                masterList.add(id);
-            }
+            masterList.addAll(ids);
         }
         threadPool.close();
-        System.out.println("Master Size: " + masterList.size());
-        System.out.println("Duplicates Size: " + duplicates.size());
-        System.out.println("All Data returned");
-    }
+        // Group by Duplicates
+        List<SnfId> duplicates = masterList.stream()
+                .collect(Collectors.groupingBy(SnfId::hashCode))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .flatMap(entry -> entry.getValue().stream())
+                .toList();
 
-    public static void basic() throws Exception {
-        MachineId machineId = new MachineId(MachineId.DEF_MACHINE_ID_BITS, NetworkInterfaceMachineIdProvider.INSTANCE);
-        SnfIdConfig snfIdConfig = new DefaultSnfIdConfigImpl(DefaultSnfIdConfigImpl.DEF_EPOCH, machineId, DefaultSnfIdConfigImpl.DEF_SEQUENCE_BITS);
-        SnfIdGen.registerIdCreator(snfIdConfig);
-        Long id = SnfIdGen.getId();
-        System.out.println("id:" + id);
-        System.out.println("snfId:" + SnfIdGen.getSnfId(id));
-        id = SnfIdGen.getId(SnfIdGen.getSnfId(id));
-        System.out.println("id::::::" + id);
+        for (SnfId snfId : duplicates) {
+            System.out.println("Duplicate Id: " + SnfIdGen.getId(snfId) + " snfId:" + snfId);
+        }
+        System.out.println("Master Size: " + masterList.size());
+        System.out.println("Duplicates:" + duplicates.size());
+        System.out.println("All Data returned");
     }
 }
