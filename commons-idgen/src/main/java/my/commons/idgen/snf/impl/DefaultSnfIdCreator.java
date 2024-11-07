85eadf3cbbec688136812568974c7d85567cdef6
Avdhesh <avdy405@gmail.com>
2024-11-07 14:21:14 +0530

    fix

--- commons-idgen/src/main/java/my/commons/idgen/snf/impl/DefaultSnfIdCreator.java
+++ commons-idgen/src/main/java/my/commons/idgen/snf/impl/DefaultSnfIdCreator.java
@@ -7,16 +7,23 @@ import my.commons.idgen.snf.SnfIdCreator;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class DefaultSnfIdCreator implements SnfIdCreator {
-
-    private final SnfIdConfig snfIdConfig;
+    private final long epoch;
+    private final long machineIdBits;
+    private final long machineId;
+    private final long sequenceBits;
     private final long sequenceMask;
+    private final long timeStampLeftShift;
 
-    private long lastTimestamp = -1L;
+    private final AtomicLong lastTimestamp = new AtomicLong(-1L);
     private final AtomicLong sequence = new AtomicLong(0L);
 
     public DefaultSnfIdCreator(SnfIdConfig snfIdConfig) {
-        this.snfIdConfig = snfIdConfig;
+        this.epoch = snfIdConfig.getEpoch();
+        this.machineIdBits = snfIdConfig.getMachineIdBits();
+        this.machineId = snfIdConfig.getMachineId();
+        this.sequenceBits = snfIdConfig.getSequenceBits();
         this.sequenceMask = snfIdConfig.getSequenceMask();
+        this.timeStampLeftShift = machineIdBits + sequenceBits;
     }
 
     /**
@@ -25,20 +32,21 @@ public class DefaultSnfIdCreator implements SnfIdCreator {
      */
     public synchronized Long createId() throws IdGenException {
         long currentTimeStamp = System.currentTimeMillis();
-        if (currentTimeStamp < lastTimestamp) {
-            throw new IdGenException("Clock moved backwards.  Refusing to generate id for " + (lastTimestamp - currentTimeStamp) + " milliseconds.");
+        long lastTs = lastTimestamp.get();
+        if (currentTimeStamp < lastTs) {
+            throw new IdGenException("Clock moved backwards.Refusing to generate id");
         }
-        if (lastTimestamp == currentTimeStamp) {
+        if (currentTimeStamp == lastTs) {
             long seq = sequence.incrementAndGet() & sequenceMask;
             if (seq == 0) {
                 sequence.set(0L);
-                currentTimeStamp = tilNextMillis(lastTimestamp);
+                currentTimeStamp = tilNextMillis(lastTs);
             }
         } else {
             sequence.set(0L);
         }
-        lastTimestamp = currentTimeStamp;
-        return ((currentTimeStamp - snfIdConfig.getEpoch()) << (snfIdConfig.getMachineIdBits() + snfIdConfig.getSequenceBits())) | (snfIdConfig.getMachineId() << snfIdConfig.getSequenceBits()) | sequence.get();
+        lastTimestamp.set(currentTimeStamp);
+        return ((currentTimeStamp - epoch) << timeStampLeftShift) | (machineId << sequenceBits) | sequence.get();
     }
 
     private long tilNextMillis(long lastTimestamp) {
--- commons-idgen/src/test/java/my/commons/idgen/snf/SnowflakeIdTest.java
+++ commons-idgen/src/test/java/my/commons/idgen/snf/SnowflakeIdTest.java
@@ -17,7 +17,7 @@ public class SnowflakeIdTest {
         int noOfIds = 10000;
         ExecutorService threadPool = Executors.newVirtualThreadPerTaskExecutor();
         ArrayList<Future<ArrayList<SnfId>>> futures = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             Future<ArrayList<SnfId>> future = threadPool.submit(new IdGenInitializationWorker("Worker " + i, idInitLatch, noOfIds));
             futures.add(future);
         }
@@ -43,7 +43,7 @@ public class SnowflakeIdTest {
         }
 
         for (SnfId snfId : masterList) {
-            //System.out.println("Master Id: " + SnfIdGen.getId(snfId) + " snfId:" + snfId);
+            System.out.println("Master Id: " + SnfIdGen.getId(snfId) + " snfId:" + snfId);
         }
         System.out.println("Master Size: " + masterList.size());
         System.out.println("Duplicates:" + duplicates.size());
